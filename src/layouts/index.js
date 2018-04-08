f699d5b35d7d95d127f48dcb234a27a3ef744e07
Jon Ambas <jon@jonambas.com>
2018-04-08 17:57:10 -0400

    Fix css build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { withPrefix } from 'gatsby-link'
 import { Footer } from 'components';
 
 import './global.scss'
@@ -10,6 +11,7 @@ const TemplateWrapper = ({ children }) => (
     <Helmet>
       <title>Jon Ambas</title>
       <meta name='description' content='Designer, Front End Engineer'/>
+      <link rel="icon" type="image/png" href={withPrefix('favicon.png')}/>
     </Helmet>
     { children() }
     <Footer/>
